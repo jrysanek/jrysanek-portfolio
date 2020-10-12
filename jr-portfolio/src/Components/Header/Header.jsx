4da5f274baadb0c07c74e1e6850ff8a4a3f75dd4
Jenna Rysanek <jennarysanek@Jennas-MacBook-Pro.local>
2020-10-12 17:15:47 -0500

    stlying Header and App components

--- jr-portfolio/src/Components/Header/Header.jsx
+++ jr-portfolio/src/Components/Header/Header.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import './Header.css';
 
 export default function Header() {
     return (
-        <div className="header">
-            <h1>Jenna Rysanek</h1>
-            <h6>Software Enginner</h6>
-            <nav>
+        <>
+            <div className="header">
+                <h1>Jenna Rysanek</h1>
+                <h6>Software Enginner</h6>
+            </div> 
+
+            <div className="nav-bar">
                 <p>Biography</p>
                 <p>Projects</p>
                 <p>Contact</p>
-            </nav>
-        </div>
+            </div>
+        </>
     )
 }
