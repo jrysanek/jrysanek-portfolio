c61721a9fd0a5da0bf91909037dd85d9c8046812
Jenna Rysanek <jennarysanek@jennas-mbp.lan>
2020-10-30 20:07:37 -0400

    Styling all components

--- jr-portfolio/src/Components/Projects/Projects.jsx
+++ jr-portfolio/src/Components/Projects/Projects.jsx
@@ -7,23 +7,23 @@ export default function Projects() {
             
             <h5>Projects</h5>
             <div className="projects-box">
-                <div className="p1">
+                {/* <div className="p1"> */}
                     <a href="https://corkit-41ce4e.netlify.app">
                     <img className="p-img" src="https://i.imgur.com/ZMKsqhh.png" alt="Cork It" />
                     </a>
-                </div>
+                {/* </div> */}
 
-                <div className="p2">
+                {/* <div className="p2"> */}
                     <a href="https://apollo-fresh-direct.surge.sh/">
                         <img className="p-img" src="https://i.imgur.com/2Hbt4pf.png" alt="Fresh Direct Rebuild" />
                     </a>
-                </div>
+                {/* </div> */}
 
-                <div className="p3">
+                {/* <div className="p3"> */}
                     <a href="https://pagebypage.netlify.app">
                         <img className="p-img" src="https://i.imgur.com/9mEHHTN.png" alt="Page by Page" />
                     </a>
-                </div>
+                {/* </div> */}
             </div>     
         </div>
     )
