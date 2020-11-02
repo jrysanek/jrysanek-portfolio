408c85e2e290cf79bbde80e16a925b1e7770c814
Jenna Rysanek <jennarysanek@jennas-mbp.lan>
2020-11-01 20:54:19 -0500

    Added image links to Contact Component

--- jr-portfolio/src/Components/Contact/Contact.jsx
+++ jr-portfolio/src/Components/Contact/Contact.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
+import './Contact.css';
 
 export default function Contact() {
     return (
         <div className="contact">
-            <p>linkedin</p>
-            <p>github</p>
+            <a href="https://www.linkedin.com/in/jenna-rysanek/">
+                <img src="https://i.imgur.com/JuW3ghb.png" />
+            </a>
+            <a href="https://github.com/jrysanek/">
+                <img src="https://i.imgur.com/Ee2TLLY.png" className="github"/>
+            </a>
         </div>
     )
 }
