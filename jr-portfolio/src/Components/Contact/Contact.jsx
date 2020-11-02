f6df50158f725f10aa901c1bc0e19cd52332047a
Jenna Rysanek <jennarysanek@jennas-mbp.lan>
2020-11-01 23:59:32 -0500

    Final Colors choosen Added new color scheme to each component

--- jr-portfolio/src/Components/Contact/Contact.jsx
+++ jr-portfolio/src/Components/Contact/Contact.jsx
@@ -3,13 +3,13 @@ import './Contact.css';
 
 export default function Contact() {
     return (
-        <div id="Contact" className="contact">
+        <footer id="Contact" className="contact">
             <a href="https://www.linkedin.com/in/jenna-rysanek/">
                 <img src="https://i.imgur.com/JuW3ghb.png" />
             </a>
             <a href="https://github.com/jrysanek/">
                 <img src="https://i.imgur.com/Ee2TLLY.png" className="github"/>
             </a>
-        </div>
+        </footer>
     )
 }
