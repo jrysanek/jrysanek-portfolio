ba5661f0377000566679a18e69855207bdcfaa03
Jenna Rysanek <jennarysanek@jennas-mbp.lan>
2020-11-02 15:14:34 -0500

    Fixed footer alignment

--- jr-portfolio/src/Components/Contact/Contact.jsx
+++ jr-portfolio/src/Components/Contact/Contact.jsx
@@ -4,7 +4,7 @@ import './Contact.css';
 export default function Contact() {
     return (
         <footer id="Contact" className="contact">
-            <a href="https://www.linkedin.com/in/jenna-rysanek/">
+            <a href="https://www.linkedin.com/in/jenna-rysanek/" className="linkedin">
                 <img src="https://i.imgur.com/JuW3ghb.png" />
             </a>
             <a href="https://github.com/jrysanek/">
